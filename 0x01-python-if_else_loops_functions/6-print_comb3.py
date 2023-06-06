305d16fe5a5f7d0417a970b373a2285c575519e5
jerrynabango <jerrynabango9@gmail.com>
2023-06-06 07:10:37 -0700

    prints uppercase

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,4 @@
 for numbers in range(8):
     for digit in range(numbers + 1, 10):
         print("{:d}{:d}".format(numbers, digit), end = ", ")
-print("{:d}{:d}".format(numbers + 1, digit))#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+print("{:d}{:d}".format(numbers + 1, digit))
