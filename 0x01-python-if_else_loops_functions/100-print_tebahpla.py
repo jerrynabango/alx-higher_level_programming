7a9ee94d9a4c425d50045dd15b45fd600f1c69b5
jerrynabango <jerrynabango9@gmail.com>
2023-06-06 08:00:59 -0700

    ASCII reverse,lower&upper case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alpha_reverse in range(26):
     if alpha_reverse % 2 == 0:
-        print("{:c}".format(122-alpha_reverse), end = "")
+        print("{:c}".format(122-alpha_reverse), end="")
     else:
-        print("{:c}".format(90-alpha_reverse), end = "")
+        print("{:c}".format(90-alpha_reverse), end="")
