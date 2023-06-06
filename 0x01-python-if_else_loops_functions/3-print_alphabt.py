33f7c900eec8d86284796f4eda454e916a5768c4
jerrynabango <jerrynabango9@gmail.com>
2023-06-06 06:36:09 -0700

    print all chr except e & q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97,123):
-        if (alphabet != ord("q") and alphabet != ord("e")):
-            print(chr(alphabet), end='')
-#ord- returns uniocode characters
+for alphabet in range(97, 123):
+    if (alphabet != ord("q") and alphabet != ord("e")):
+        print(chr(alphabet), end='')
