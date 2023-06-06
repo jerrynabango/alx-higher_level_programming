0deb96d8b3afe8c3d1b2abe1e69726f51f358c1b
jerrynabango <jerrynabango9@gmail.com>
2023-06-06 07:53:16 -0700

    print n.o's separated by space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-        if number % 3*5 == 0:
-            print("Fizzbuzz", end=" ")
+        if number % (3*5) == 0:
+            print("Fizzbuzz ", end="")
         elif number % 3 == 0:
-            print("Fizzbuzz", end=" ")
+            print("Fizz ", end="")
         elif number % 5 == 0:
-            print("Fizzbuzz", end=" ")
+            print("Buzz ", end="")
         else:
-            print("{:d}".format(number), end=" ")
+            print("{:d} ".format(number), end="")
