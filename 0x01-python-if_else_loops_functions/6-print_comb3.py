80a573fd1c529353c7d3ea5e95b5992cdd4ea4c8
jerrynabango <jerrynabango9@gmail.com>
2023-06-06 11:53:14 +0300

    print combinations of digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for numbers in range(8):
+    for digit in range(numbers + 1, 10):
+        print("{:d}{:d}".format(numbers, digit), end = ", ")
+print("{:d}{:d}".format(numbers + 1, digit))
