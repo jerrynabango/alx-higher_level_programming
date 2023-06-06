7b771bd06a07b73e1664ceab50313278b47d818b
jerrynabango <jerrynabango9@gmail.com>
2023-06-06 14:32:37 +0300

    copy of strfing & remove character at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def remove_char_at(str, n):
+    character = 0
+    copy = ""
+    for chr in str:
+        if character != n:
+            copy += chr
+        character += 1
+    return copy
