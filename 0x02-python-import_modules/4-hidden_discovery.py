b75db408fbcb86df3ce023c5735d8ddc2f05ee27
Jerry Nabango <jerrynabango9@gmail.com>
2023-06-08 23:27:15 +0300

    compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import hidden_4
-for hidden in dir(hidden_4):
-    if hidden[:2] != "__":
-        print("{hidden}".format(hidden))
+    import hidden_4
+    for hidden in dir(hidden_4):
+        if hidden[:2] != "__":
+            print("{}".format(hidden))
