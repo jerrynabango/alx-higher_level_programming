6bd579b5faf4fbdcd16258ce4318a5857c08479d
Jerry Nabango <jerrynabango9@gmail.com>
2023-06-08 23:38:06 +0300

    my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     if argc != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
     if argv[2] in operator:
         integer1 = int(argv[1])
         integer2 = int(argv[3])
         operation = operators[argv[2]]
         result = operation(integer1, integer2)
-        print("{:d} {:d} {:d} = {:d}".format(integer1, argv[2], integer2, result))
+        print('{:d} {:d} {:d} = {:d}'.format(integer1, argv[2], integer2, result))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
     exit(0)
