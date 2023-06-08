ef709ab014ac35e55a1db5a3d8b26b8f7b22b308
Jerry Nabango <jerrynabango9@gmail.com>
2023-06-09 00:16:23 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-    def calculate_result(num1, operator, num2):
+
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+def calculate_result(num1, operator, num2):
     if operator == '+':
         return add(num1, num2)
     elif operator == '-':
@@ -14,7 +16,7 @@
         return div(num1, num2)
     else:
         raise ValueError("Unknown operator. Available operators: +, -, * and /")
-        
+
 if __name__ == "__main__":
     if len(argv) != 4:
         print('Usage: {} <a> <operator> <b>'.format(argv[0]))
