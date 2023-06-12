9036b0a172517c450e1300a464ab64bf145986b6
Jerry Nabango <jerrynabango9@gmail.com>
2023-06-12 11:58:45 +0300

    find biggest number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return
     biggest_number = my_list[0]
     for number in my_list:
         if number > biggest_number:
