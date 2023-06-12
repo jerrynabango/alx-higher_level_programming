30437713943d4c2a4274cd807a6c625149def51d
Jerry Nabango <jerrynabango9@gmail.com>
2023-06-12 11:32:22 +0300

    replace element @ specific position

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        replace[idx] = element
         replace = my_list.copy()
+        replace[idx] = element
+
         return replace
     return my_list.copy
