6201dc99bac622f286d473813b92aa0be0d93b19
jerrynabango <jerrynabango9@gmail.com>
2023-06-14 19:34:52 +0300

    compute square values of integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/bin/bash/python3
 def square_matrix_simple(matrix=[]):
     return [list(map((lambda square:square * square), elm)) for elm in matrix]
-
-
-"""
-elm: Indicates an element in the matrix
-"""
