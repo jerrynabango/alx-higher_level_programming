93488357db1f7df49485bbb385c428e6a02e71a8
jerrynabango <jerrynabango9@gmail.com>
2023-06-14 20:09:52 +0300

    weighted average of integers

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list:
-        integter1 = 0
-        integer2 = 0
-        for tup in my_list:
-            integter1 += (tup[0] * tup[1])
-            integer2 += tup[1]
-        return integter1 / integer2
+        return 0
+    integter1 = 0
+    integer2 = 0
+    for tup in my_list:
+        integter1 += (tup[0] * tup[1])
+        integer2 += tup[1]
+    return integter1 / integer2
