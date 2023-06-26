f10ba85d6cf7ae5c0231ed118f2e9de5ee4fff80
jerrynabango <jerrynabango9@gmail.com>
2023-06-26 15:56:14 +0300

    bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
     byte = 0
-
     for code in range(1, 3):
         try:
             if code > a:
                 raise Exception('Too far')
-
             byte += a ** b / code
-        except:
+        except ValueError:
             byte = b + a
             break
-
     return byte
