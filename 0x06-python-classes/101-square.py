8a15b8387353b73b8e14c1bd54039aee33203db5
jerrynabango <jerrynabango9@gmail.com>
2023-06-27 22:00:11 +0300

    print square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-"""Print Square instance"""
+
+"""
+A script that creates a class and validates its given attribute"""
 
 
 class Square:
+    """
+        Atrributes:
+    """
     def __init__(self, size=0, position=(0, 0)):
-        """Initialization of the Square"""
+        """
+        Args:
+            self (Square): object
+            size (int): size of square
+            position (tuple): print coordinates
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
         """
-        @property: Defines the method(Getter for specific attribute).
-        private attribute: __size
+        size getter
+        Returns:
+            int: size of Square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Size of the square"""
+        """
+        size setter
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is -ve
+        """
         if isinstance(value, int) is False:
             raise TypeError('size must be an integer')
         if value < 0:
@@ -28,43 +44,64 @@ class Square:
     @property
     def position(self):
         """
-        @property: Defines the method(Getter for specific attribute).
-        private attribute: __position
+        position getter
+        Returns:
+            tuple: Size of Square
         """
         return self.__position
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        """
+        position setter
+        Args:
+            value (tuple): New Square position
+        Raises:
+            TypeError: if value is not a tuple and elements are not integers
+        """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """
+        Args:
+            self (Square): instance of class
+        Return:
+            area (int)
+        """
+        return (self.size * self.size)
 
     def my_print(self):
-        """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        """
+        Print the Square to stdout using '#'
+        """
+        if self.size == 0:
+            print()
+        else:
+            if self.position[1] > 0:
+                for i in range(self.position[1]):
+                    print('\n', end="")
+            for i in range(self.size):
+                print("{}{}".format(' ' * self.position[0], '#' * self.size))
 
     def __str__(self):
-        """Define the print() representation of a Square."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+        """
+        String representation of instances
+        """
+        string = ""
+        if self.size == 0:
+            return string
+        else:
+            if self.position[1] > 0:
+                for i in range(self.position[1]):
+                    string += '\n'
+            for i in range(self.size):
+                string += ' ' * self.position[0]
+                string += '#' * self.size
+                string += '\n'
+            return string[:-1]
