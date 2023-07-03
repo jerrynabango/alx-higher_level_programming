ce9112d11432c89358a6547599e957ab9f51abf1
jerrynabango <jerrynabango9@gmail.com>
2023-07-03 14:32:51 +0300

    define rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,15 +15,11 @@ class Rectangle:
         """Getter method for width"""
         return self.__width
 
-    @property
-    def height(self):
-        """Getter method for height"""
-        return self.__height
     @width.setter
     def width(self, value):
         """Setter method for the width
         private attribute: __width"""
-        if type(value) is not int:
+        if type(value) is not int:  # Identify the data type if its an integer
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -33,7 +29,7 @@ class Rectangle:
     def height(self, value):
         """Setter method for height
         private attribute: __height"""
-        if type(value) is not int:
+        if type(value) is not int:  # Identify the data type if its an integer
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
