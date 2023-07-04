6b01090ef4dbe8220b5ed3ded6400f446e4ee5cb
jerrynabango <jerrynabango9@gmail.com>
2023-07-04 15:25:32 +0300

    str representation

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string(pythonic=[]):
-    """Function that returns string representation"""
     pythonic += ["BestSchool"]  # Concatenation between Pythonic and Pythonic
     return ", ".join(pythonic)
