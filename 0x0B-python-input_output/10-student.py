fe5b40b22ff13f4a55c85a3317ed2241c7cf0be6
jerrynabango <jerrynabango9@gmail.com>
2023-07-11 12:02:28 +0300

    retrieves a dictionary representation of a Student instance

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""""Student to JSON with filter"""
+
+
+class Student:
+    """Defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) == list:
+            props = {a: self.__dict__[a]
+                     for a in attrs if a in self.__dict__}
+            return props
+        else:
+            return self.__dict__
