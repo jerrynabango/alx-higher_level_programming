3247f2c6f34d031079d49d54831f205906f599bb
jerrynabango <jerrynabango9@gmail.com>
2023-08-15 03:15:13 -0700

    create table with multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 INSERT INTO second_table(id, name, score) VALUES(1, "John", 10);
 INSERT INTO second_table(id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO second_table(id, name, score) VALUES(3, "Bob", 4);
+INSERT INTO second_table(id, name, score) VALUES(3, "Bob", 14);
 INSERT INTO second_table(id, name, score) VALUES(4, "George", 8);
 
