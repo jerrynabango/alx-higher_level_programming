2f1387a0f0a2e85bf98d022b5c24a395e933ef62
jerrynabango <jerrynabango9@gmail.com>
2023-08-15 04:10:37 -0700

    average score

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- script computes score average of all records in second_table of db hbtn_0c_0
-SELECT AVG(score) AS score_average FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
 
