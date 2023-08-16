b8927de248484db263369301c2b785770f73bd68
jerrynabango <jerrynabango9@gmail.com>
2023-08-16 21:26:33 +0300

    lists all shows by ratings

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,13 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT title, SUM(rate)
-FROM tv_shows
-INNER JOIN tv_show_ratings
-ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY itle
-ORDER BY rating DESC;
+SELECT
+    title, SUM(rate) AS rating
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_ratings
+ON
+    tv_shows.id = tv_show_ratings.show_id
+GROUP BY
+    title
+ORDER BY
+    rating DESC;
