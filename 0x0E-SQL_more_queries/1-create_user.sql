9d9bd0a0c8c31c68fc32133882d3de31bf452251
jerrynabango <jerrynabango9@gmail.com>
2023-08-16 14:28:25 +0300

    create user, pwd and grant permissions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,4 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';                                                  
 GRANT ALL PRIVILEGES ON *.* TO 'user user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
