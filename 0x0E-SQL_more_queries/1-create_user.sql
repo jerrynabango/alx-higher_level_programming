654d14d0ed48ac2ef274d9176490aed279caa33d
jerrynabango <jerrynabango9@gmail.com>
2023-08-16 14:39:53 +0300

    create user, pwd and grant permissions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,4 @@
 -- user_0d_1 should have all privileges on your MySQL server
 -- The user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';                                                  
-
--- Have full control over the databases & tables                                
 GRANT ALL PRIVILEGES ON *.* TO 'user user_0d_1'@'localhost';
