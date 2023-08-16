ef82f0ece206926dfa6fb66838a5d3a4ec9486a5
jerrynabango <jerrynabango9@gmail.com>
2023-08-16 14:39:04 +0300

    create user, pwd and grant permissions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
--- script to run msql commands
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost
+-- script that creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';                                                  
+
+-- Have full control over the databases & tables                                
+GRANT ALL PRIVILEGES ON *.* TO 'user user_0d_1'@'localhost';
