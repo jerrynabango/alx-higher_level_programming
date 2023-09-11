bc3e0df90ef5cbbee116c2045bc19cdfd922193c
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 19:54:20 +0300

    prints message depending on n.o of args passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,11 +7,11 @@
  * Otherwise, it prints "Arguments found."
  */
 
-const message = process.argv.length - 2;
+const numberOfArguments = process.argv.length - 2;
 // Strict Equality comparison (===)
-if (message === 0) {
+if (numberOfArguments === 0) {
 	console.log('No argument');
-} else if (message === 1) {
+} else if (numberOfArguments === 1) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
