7cde5864849ea96c6ddfddd0c9a75c80894b5280
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 22:25:09 +0300

    searches second biggest integer in list of args

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
 
-function factorial (n) {
-	if (isNaN(n) || n === 1) { return (1); }
-	return n * factorial(n - 1);
+/**
+ * script that computes and prints a factorial
+ * The first argument is integer (argument can be cast as integer) used for computing the factorial
+ * Factorial of NaN is 1
+ */
+
+function factorial (integer) {
+	if (isNaN(integer) || integer === 1) { return (1); }
+	return integer * factorial(integer - 1);
   }
   
-  console.log(factorial(process.argv[2]));
+console.log(factorial(process.argv[2]));
