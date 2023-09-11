973f414072b6b45c199cb1d87ab38e791cd4c051
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 11:49:37 +0300

    print n.o of args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+/**
+* script that prints a message depending of the number of arguments passed:
+
+* If no arguments are passed to the script, print “No argument”
+* If only one argument is passed to the script, print “Argument found”
+* Otherwise, print “Arguments found”
+*/
+
+const numberOfArguments = process.argv.length - 2;
+
+// Strict Equality comparison (===)
+if (numberOfArguments === 0) {
+	console.log('No argument');
+} else if (numberOfArguments === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
