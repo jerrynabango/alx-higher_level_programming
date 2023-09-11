7c5bd867d4738e82a00accefefdcafa6ec9f0877
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 21:09:13 +0300

    prints number if arg converted to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
-/**
- * script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
- * If the argument can’t be converted to an integer, print “Not a number”
- */
+const arg = parseInt(process.argv[2]);
 
-//parseInt: function that parses a string and returns a number
-const integer = parseInt(process.argv[2]);
-
-if (Number.isNaN(integer)) {
+if (Number.isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${integer}`);
+  console.log(`My number: ${arg}`);
 }
