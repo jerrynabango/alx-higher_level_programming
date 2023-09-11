898637a302f1f529a54bb3b7e9ab0e69960362ef
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 12:05:26 +0300

    prints args passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * script that prints the first argument passed to it:
+ * If no arguments are passed to the script, print “No argument”
+ * You must use console.log(...) to print all output
+ */
+
+const valueOfArgument = process.argv[2];
+
+if (valueOfArgument === undefined) {
+	console.log('No argument');
+} else {
+	console.log(valueOfArgument);
+}
