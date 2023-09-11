15c40f26b3902bebced446e7eafa0eb0ffbcc6a0
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 22:30:52 +0300

    print factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,6 +3,6 @@
 function search (arr) {
 	if (arr.length < 2) { return (0); }
 	return arr.map(Number).sort((x, y) => x - y)[arr.length - 2];
-}
+  }
   
-console.log(search(process.argv.slice(2)));
+  console.log(search(process.argv.slice(2)));
