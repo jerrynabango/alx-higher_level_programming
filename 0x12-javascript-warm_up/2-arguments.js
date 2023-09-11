2931245b9aa331418029e258230de4d53b4fdeed
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 12:34:02 +0300

    prints first argument passed to it

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,6 @@
 
 /**
 * script that prints a message depending of the number of arguments passed:
-
 * If no arguments are passed to the script, print “No argument”
 * If only one argument is passed to the script, print “Argument found”
 * Otherwise, print “Arguments found”
