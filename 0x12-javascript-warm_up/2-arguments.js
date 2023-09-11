d4581c60ca9406366d44a39ab7490a719b95e866
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 12:44:49 +0300

    prints first argument passed to it

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 /**
-* script that prints a message depending of the number of arguments passed:
-* If no arguments are passed to the script, print “No argument”
-* If only one argument is passed to the script, print “Argument found”
-* Otherwise, print “Arguments found”
-*/
+ * A script that prints a message depending on the number of arguments passed:
+ * - If no arguments are passed to the script, it prints "No argument."
+ * - If only one argument is passed to the script, it prints "Argument found."
+ * - Otherwise, it prints "Arguments found."
+ */
 
-// Strict Equality comparison (===)
 const numberOfArguments = process.argv.length - 2;
+// Strict Equality comparison (===)
 if (numberOfArguments === 0) {
 	console.log('No argument');
 } else if (numberOfArguments === 1) {
