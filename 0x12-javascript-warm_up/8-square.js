a0206110f43b052aebd2090476e5b75262f30b6e
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 21:40:12 +0300

    prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -8,15 +8,12 @@
 
 // math.floor: function used to convert a number to an integer value
 const square = Math.floor(Number(process.argv[2]));
-
 if (isNaN(square)) {
-  console.log('Missing square');
+  console.log('Missing size');
 } else {
   for (let arg1 = 0; arg1 < square; arg1++) {
-    let size = '';
-    for (let arg2 = 0; arg2 < square; arg2++) {
-      size += 'X';
-    }
-    console.log(size);
+    let sizeOfSquare = '';
+    for (let arg2 = 0; arg2 < square; arg2++) sizeOfSquare += 'X';
+    console.log(sizeOfSquare);
   }
 }
