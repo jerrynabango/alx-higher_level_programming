5ae814444c8e187fc85a72d2c722b90583d06c14
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 19:41:52 +0300

    prints message depending on n.o of args passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 
-const numberOfArguments = process.argv.length - 2;
+/**
+ * A script that prints a message depending on the number of arguments passed:
+ * If no arguments are passed to the script, it prints "No argument."
+ * If only one argument is passed to the script, it prints "Argument found."
+ * Otherwise, it prints "Arguments found."
+ */
+
+const args = process.argv.length - 2;
 // Strict Equality comparison (===)
-if (numberOfArguments === 0) {
-	console.log('No argument');
-} else if (numberOfArguments === 1) {
-	console.log('Argument found');
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
