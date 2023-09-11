7c0617e10607ac27ddcec4780d770429e70c14dd
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 11:50:32 +0300

    prints 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-/*
-script that prints 3 lines:
+/**
+* script that prints 3 lines:
 
-The first line: “C is fun”
-The second line: “Python is cool”
-The third line: “JavaScript is amazing”
+* The first line: “C is fun”
+* The second line: “Python is cool”
+* The third line: “JavaScript is amazing”
 */
-// Using template literal
 
+// Using template literal
 console.log(`C is fun
 Python is cool
 JavaScript is amazing`);
