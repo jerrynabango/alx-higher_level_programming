5855f6a9c7c323ad457ed5c56e4efc9bf156cf40
jerrynabango <jerrynabango9@gmail.com>
2023-09-11 22:13:22 +0300

    print factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
 
+/**
+ * script that computes and prints a factorial
+ * The first argument is integer (argument can be cast as integer) used for computing the factorial
+ * Factorial of NaN is 1
+ */
+
 function factorial (integer) {
 	if (isNaN(integer) || integer === 1) { return (1); }
 	return integer * factorial(integer - 1);
   }
   
-console.log(factorial(process.argv[2]));
+  console.log(factorial(process.argv[2]));
