eeab3b3e42485ef96c6b0695fa6e76d67a1f631e
jerrynabango <jerrynabango9@gmail.com>
2023-09-12 22:05:42 +0300

    returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-// function that returns the reversed version of a list
 exports.esrever = function (list) {
 	const reverse = [];
-	// forEach: used to reverse the list of versions of a list that are not already in the list of versions
-	// unshift: used to add the first element to the end of the list and remove from the beginning of the list
-	list.forEach(reversedVersion => reverse.unshift(reversedVersion));
+	list.forEach(e => reverse.unshift(e));
 	return reverse;
-};
+  };
