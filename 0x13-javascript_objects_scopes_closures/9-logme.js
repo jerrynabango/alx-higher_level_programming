b4d495bd97e79e15aa1ab2d2c96cb1b541889d97
jerrynabango <jerrynabango9@gmail.com>
2023-09-12 22:01:21 +0300

    prints number of args already printed and the new arg value

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
-// function that prints the number of arguments already printed and the new argument value
-let numberOfArgs = 0;
+let narg = 0;
 
 exports.logMe = function (item) {
-	// using template literal 
-	console.log(`${numberOfArgs++}: ${item}`);
+  console.log(narg + ': ' + item);
+  narg++;
 };
