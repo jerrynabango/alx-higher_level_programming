d7a32dde73aa7cbd05d8bb32fb4b3f2e60b2fa92
jerrynabango <jerrynabango9@gmail.com>
2023-09-16 20:27:30 +0300

    lists all states with a name starting with N (upper N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 import sys
 
 
-def main():
+def Filter_states():
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
@@ -29,4 +29,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    Filter_states()
