d17ea5aaf9ac011ebfef1a31c361bfee0d645d23
jerrynabango <jerrynabango9@gmail.com>
2023-09-17 21:42:28 +0300

    lists all states objects from db

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,16 +9,13 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def all_states():
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for states in session.query(State).order_by(State.id):
-        print(states.id, states.name, sep=": ")
-
-
-if __name__ == "__main__":
-    all_states()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
