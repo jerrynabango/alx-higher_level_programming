e951082a735014e13b52cb4148d134ada3c120dd
jerrynabango <jerrynabango9@gmail.com>
2023-09-17 23:22:58 +0300

    relations

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""
+city relationship
 """
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
