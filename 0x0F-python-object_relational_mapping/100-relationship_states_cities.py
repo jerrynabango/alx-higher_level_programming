95943eb8a18da64302fc21bd0737b303f7edffb8
jerrynabango <jerrynabango9@gmail.com>
2023-09-17 23:08:49 +0300

    relations(states, cities

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Cities in state
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+
+def city_relations():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = State(name='California')
+    cities = City(name='San Francisco')
+    states.cities.append(cities)
+
+    session.add(states)
+    session.add(cities)
+    session.commit()
+
+
+if __name__ == "__main__":
+    city_relations
