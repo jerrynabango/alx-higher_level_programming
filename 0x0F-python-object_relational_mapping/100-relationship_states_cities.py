76db06a55dc2a8ec0cdc45cbbc70d4a104c85b0a
jerrynabango <jerrynabango9@gmail.com>
2023-09-24 22:04:08 +0300

    city

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-Cities in state
-"""
-
-from model_state import State, Base
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""Creates the State "California" with the City "San Francisco" from a DB"""
 import sys
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-def City_relationship():
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
@@ -19,14 +16,10 @@ def City_relationship():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = State(name='California')
-    cit = City(name='San Francisco')
-    state.cities.append(cit)
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
 
-    session.add(state)
-    session.add(cit)
+    session.add(newState)
+    session.add(newCity)
     session.commit()
-
-
-if __name__ == '__main__':
-    City_relationship()
