49c0c374312b4d0bf7af9227e8a4c9106d267989
jerrynabango <jerrynabango9@gmail.com>
2023-09-24 13:55:41 +0300

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+SQL Injection..."""
+
+import sys
+import MySQLdb
+
+
+def sql():
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    sql():
