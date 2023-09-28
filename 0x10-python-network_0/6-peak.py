7fcf830db5bc3226f89881202efce336cc4b6c1b
jerrynabango <jerrynabango9@gmail.com>
2023-09-28 14:59:47 +0300

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 """
-Find a peak
+A Find a peak
 """
 
 
 def find_peak(list_of_integers):
     """
-    Defines the peak function for a list of Integers
+     Defines the peak of a list of Integers
     """
     if list_of_integers != []:
         peak_1, peak_2 = 0, len(list_of_integers) - 1
-        for _ in range(len(list_of_integers)):
-            ls = peak_1 + (peak_2 - peak_1) // 2
-            if list_of_integers[ls] < list_of_integers[ls + 1]:
+        while peak_1 < peak_2:
+            ls = peak_1 + (peak_2-peak_1) // 2
+            if list_of_integers[ls] < list_of_integers[ls+1]:
                 peak_1 = ls + 1
             else:
                 peak_2 = ls
