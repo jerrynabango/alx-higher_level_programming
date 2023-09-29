1e946a62ac65367485f5a1d8bd82f7837422b64d
jerrynabango <jerrynabango9@gmail.com>
2023-09-29 22:23:39 +0300

    sends a request to url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+def fetches():
+    import requests
+    fetch_url = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:\n\t- type: {}\n\t- content: {}".
+          format(type(fetch_url.text), fetch_url.text)
+    )
+
+
+if __name__ == '__main__':
+    fetches()
---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header
+"""
+
+import requests
+import sys
+
+
+def script():
+    url = sys.argv[1]
+    scripts = requests.get(url)
+    print(scripts.headers.get("X-Request-Id"))
+
+
+if __name__ == '__main__':
+    script()
