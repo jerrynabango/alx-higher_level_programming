c8ba41410637a47746bedaf5c23ae4a5eaa02afc
jerrynabango <jerrynabango9@gmail.com>
2023-09-29 23:14:08 +0300

    fetches url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,8 +8,7 @@ def fetches():
     import requests
     fetch_url = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:\n\t- type: {}\n\t- content: {}".
-          format(type(fetch_url.text), fetch_url.text)
-    )
+          format(type(fetch_url.text), fetch_url.text))
 
 
 if __name__ == '__main__':
