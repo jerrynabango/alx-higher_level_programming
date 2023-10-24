e14262c6f861173216f0c15b77fc61c4cdd6323f
jerrynabango <jerrynabango9@gmail.com>
2023-10-24 14:25:04 +0300

    prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie:
+// script that prints all characters of a Star Wars movie
+
 const request = require('request');
 const starWars = process.argv[2];
 const movieApi = 'https://swapi-api.hbtn.io/api/';
