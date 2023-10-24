f5a35fbe2736dc38f093e5492c02b2e3fbb48f7f
jerrynabango <jerrynabango9@gmail.com>
2023-10-24 13:44:55 +0300

    prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-// script that prints the title of a Star Wars movie where the episode number matches a given integer.
-//  Star wars API with the endpoint 
+// script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
 
 const request = require('request');
+//  Star wars API with the endpoint 
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
