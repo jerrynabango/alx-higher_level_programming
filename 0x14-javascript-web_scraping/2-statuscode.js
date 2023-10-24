b5421213b7724ff6de3cefb1f49fb4c6405ba591
jerrynabango <jerrynabango9@gmail.com>
2023-10-24 13:40:34 +0300

    display's status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 // script that display the status code of a GET request.
 
 const request = require('request');
-request.get(process.argv[2]), 'utf-8'.on('response', function (response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
