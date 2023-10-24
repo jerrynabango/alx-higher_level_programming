e470d289ef6b1324208e7503b59158526b3bb59c
jerrynabango <jerrynabango9@gmail.com>
2023-10-24 13:35:59 +0300

    reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
+// fs- File System
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
