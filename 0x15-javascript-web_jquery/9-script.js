60d3379a7128d7aae2a2cd4540199713b081e3db
jerrynabango <jerrynabango9@gmail.com>
2023-11-05 16:11:45 +0300

    displays hello

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$.get("https://fourtonfish.com/hellosalut/?lang=fr", function (say) {
-		$("DIV#hello").text(say.hello);
+	$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (say) {
+		$('DIV#hello').text(say.hello);
 	});
 });
