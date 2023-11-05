40dee0bff5168a63be432c423adbf8e539fcdccf
jerrynabango <jerrynabango9@gmail.com>
2023-11-05 15:43:06 +0300

    lists movie titles

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$.get(
+		"https://swapi-api.alx-tools.com/api/films/?format=json",
+		function (list) {
+			list.fetches.forEach(function (movies) {
+				$("<li>").text(movies.title).appendTo("ul#list_movies");
+			});
+		}
+	);
+});
