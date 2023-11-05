375c0dd916c3b6d7df32472b6ac8aa65b2adfb7a
jerrynabango <jerrynabango9@gmail.com>
2023-11-05 16:10:45 +0300

    adds, removes & clear li elements

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-	$("#add_item").click(function () {
-		$("<li>").text("Item").appendTo("ul.my_list");
+	$('#add_item').click(function () {
+		$('<li>').text('Item').appendTo('ul.my_list');
 	});
-	$("#remove_item").click(function () {
-		$("ul.my_list li:last-child").remove();
+	$('#remove_item').click(function () {
+		$('ul.my_list li:last-child').remove();
 	});
-	$("#clear_list").click(function () {
-		$("ul.my_list").empty();
+	$('#clear_list').click(function () {
+		$('ul.my_list').empty();
 	});
 });
