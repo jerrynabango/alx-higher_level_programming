687956c8a5e42e7503974c55544d4a214dfc1a77
jerrynabango <jerrynabango9@gmail.com>
2023-11-05 16:11:11 +0300

    updates text-color

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
 	const header = document.querySelector('header');
 	header.style.color = '#FF0000';
 });
